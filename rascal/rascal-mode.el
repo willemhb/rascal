a585f3b8d9c209a4a2f28eaeceb6ec432fddbac3
William <william.r.jukes@gmail.com>
2022-09-12 23:30:08 -0400

    include emacs?

---
+++ rascal/rascal-mode.el
@@ -0,0 +1,29 @@
+;;; package --- Summary:
+
+;;; keeping this here so it's in the repo
+
+;;; Commentary:
+;;; elisp doc formatting is extraordinarily fucked up
+
+;;; Code:
+(define-generic-mode
+  'rascal-mode
+  '("//")
+  '("fun" "mac" "type" "val"
+    "do" "quote"
+    "if" "else" "and" "or" "cond"
+    "end")
+  '(("otherwise" . font-lock-builtin-face)
+    ("true" . font-lock-builtin-face)
+    ("false" . font-lock-builtin-face)
+    ("nil" . font-lock-builtin-face)
+    ("none" . font-lock-builtin-face)
+    (":[-+_a-zA-Z0-9?!/]+" . font-lock-builtin-face)
+    ("@[-_a-zA-Z0-9?!/]+" . font-lock-builtin-face)
+    ("[A-Z][_a-zA-Z0-9]*" . font-lock-function-name-face))
+  '("\\.rsp$" "\\.rdn$" "\\.rdn.o$") ;; source file, serialization format, object file
+  '(rainbow-delimiters-mode)
+  "A mode for rascal files.")
+
+(provide 'rascal-mode)
+;;; rascal-mode.el ends here
