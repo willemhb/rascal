3b44b894bd0d1095d3a362245d135c31b9376b7b
Will Jukes <william.r.jukes@gmail.com>
2022-09-15 14:30:16 -0400

    -m

--- rascal/rascal-mode.el
+++ rascal/rascal-mode.el
@@ -9,15 +9,18 @@
 (define-generic-mode
   'rascal-mode
   '("#")
-  '("fun" "val" "do" "if"  "elif"
-    "else" "and" "or")
+  '("fun" "mac" "data" "type" "class" "cntl" "hndl" "with"
+    "val" "var" "do" "quote" "syntax" "where"
+    "if"  "else" "case" "cond" "and" "or" "when"
+    )
   '(("otherwise" . font-lock-builtin-face)
     ("true" . font-lock-builtin-face)
     ("false" . font-lock-builtin-face)
     ("nil" . font-lock-builtin-face)
     ("none" . font-lock-builtin-face)
     (":\\{1\\}[-+_a-zA-Z0-9?!/]+" . font-lock-builtin-face)
-    ("@[-_a-zA-Z0-9?!/]+" . font-lock-builtin-face))
+    ("@[-_a-zA-Z0-9?!/]+" . font-lock-builtin-face)
+    ("[A-Z][_a-zA-Z0-9]*" . font-lock-function-name-face))
   '("\\.rsp$" "\\.rdn$" "\\.rdn.o$") ;; source file, serialization format, object file
   '(rainbow-delimiters-mode)
   "A mode for rascal files.")
