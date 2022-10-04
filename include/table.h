31179d1389c80185baaadc523fa60dcda27ab11f
William <william.r.jukes@gmail.com>
2022-10-04 16:31:12 -0400

    -m

--- include/table.h
+++ include/table.h
@@ -3,17 +3,33 @@
 
 #include "array.h"
 
+typedef union
+{
+  int8_t  *o8;
+  int16_t *o16;
+  int32_t *o32;
+} ords_t;
+
 // describe macros
 #define TABLE_SLOTS(V)					\
   V          *data;					\
   arity_t   len;					\
   arity_t   cap
 
+#define ORDERED_TABLE_SLOTS(V)				\
+  V *data;						\
+  arity_t len;						\
+  arity_t cap;						\
+  ords_t  ord;						\
+  arity_t ocap;						\
+  arity_t osize
+
 // table describe macros
 #define ENTRY_SLOTS(K, key, V, val)			\
   K        key;						\
   V        val;						\
   hash_t hash
+
 
 #define TABLE_INIT(T, E, type)				\
   void init_##T(T##_t *T)				\
@@ -22,6 +38,15 @@
     T->len   = 0;					\
     T->cap   = MinCs[type];				\
     T->data  = alloc_vec(T->cap, E##_t* );		\
+  }
+
+#define ORDERED_TABLE_INIT(T, E, type)		\
+  void init_##T(T##_t *T)			\
+  {						\
+    init_obj(&T->obj, type, 0);			\
+    T->len = 0;					\
+    T->cap = MinCs[type];			\
+    init_ords(T->cap, &T->ord, &T->osize);	\
   }
 
 #define TABLE_TRACE(T, E)			\
@@ -38,6 +63,14 @@
     dealloc_vec(T->data, T->cap, E##_t*);	\
   }
 
+#define ORDERED_TABLE_FREE(T, E)		\
+  void free_##T(obj_t *obj)			\
+  {						\
+    T##_t* T = (T##_t*)obj;			\
+    dealloc_vec(T->data, T->cap, E##_t*);	\
+    free_ords(T->ord, T->ocap, T->osize );	\
+  }
+
 #define TABLE_CLEAR(T, E)				\
   void clear_##T(T##_t *T)				\
   {							\
@@ -46,7 +79,7 @@
   }
 
 #define TABLE_REHASH(T, E)						\
-  void rehash_##T(E##_t **old,size_t oldc,E##_t **new,size_t newc)	\
+  void rehash_##T(E##_t **old,arity_t oldc,E##_t **new,arity_t newc)	\
   {									\
     for (arity_t i=0; i<oldc; i++)					\
       {									\
@@ -65,35 +98,99 @@
       }									\
   }
 
-#define TABLE_RESIZE(T, E, type)				\
-  void resize_##T(T##_t *T, size_t newl)			\
-  {								\
-    size_t oldc = T->cap;					\
-    size_t newc = pad_table_size(T->len,newl,oldc,MinCs[type]);	\
-    if (newc != oldc)						\
-      {								\
-	E##_t **oldspace = T->data;				\
-	E##_t **newspace = alloc_vec(newc, E##_t*);		\
-	rehash_##T(oldspace, oldc, newspace, newc);		\
-								\
-	T->data = newspace;					\
-	T->cap  = newc;						\
-								\
-	dealloc_vec(oldspace, oldc, E##_t*);			\
-      }								\
-    T->len = newl;						\
+#define ORDERED_TABLE_REHASH(T, E)					\
+  void rehash_##T(E##_t **E##s, arity_t len, arity_t cap, ord_t ords)	\
+  {									\
+    if (cap < INT8_MAX)							\
+      {									\
+	for (arity_t i=0; i<len; i++)					\
+	  {								\
+	    hash_t  h = E##s[i]->hash;					\
+	    arity_t m = cap-1;						\
+	    arity_t j = h & m;						\
+	    								\
+	    while (ords.o8[j] == -1)					\
+	      j = (j+1) & m;						\
+	    								\
+	    ords[j] = i;						\
+	  }								\
+      }									\
+    else if (cap < INT16_MAX)						\
+      {									\
+	for (arity_t i=0; i<len; i++)					\
+	  {								\
+	    hash_t  h = E##s[i]->hash;					\
+	    arity_t m = cap-1;						\
+	    arity_t j = h & m;						\
+	    								\
+	    while (ords.o16[j] == -1)					\
+	      j = (j+1) & m;						\
+	    								\
+	    ords[j] = i;						\
+	  }								\
+      }									\
+    else								\
+      {									\
+	for (arity_t i=0; i<len; i++)					\
+	  {								\
+	    hash_t  h = E##s[i]->hash;					\
+	    arity_t m = cap-1;						\
+	    arity_t j = h & m;						\
+	    								\
+	    while (ords.o32[j] == -1)					\
+	      j = (j+1) & m;						\
+									\
+	    ords[j] = i;						\
+	  }								\
+      }									\
   }
 
-#define TABLE_PUT(T, E, K, key, hashfn, cmpfn)	\
-  bool T##_put(T##_t *T, K key, E##_t **buf)	\
-  {						\
-    hash_t  h  = hashfn(key);			\
-    bool      o  = false;			\
-    arity_t m  = T->cap-1;			\
-    arity_t i  = h & m;				\
-    E##_t **E##s = T->data;			\
-    E##_t  *E    = NULL;			\
-    						\
+
+#define TABLE_RESIZE(T, E, type)					\
+  void resize_##T(T##_t *T, arity_t newl)				\
+  {									\
+    arity_t oldc = T->cap, oldl = T->len;				\
+    T->len       = newl;						\
+    arity_t newc = pad_table_size(oldl, newl, oldc, MinCs[type]);	\
+    T->cap       = newc;						\
+    if (newc != oldc)							\
+      {									\
+	E##_t **oldspace = T->data;					\
+	E##_t **newspace = alloc_vec(newc, E##_t*);			\
+	rehash_##T(oldspace, oldc, newspace, newc);			\
+									\
+	T->data = newspace;						\
+	T->cap  = newc;							\
+									\
+	dealloc_vec(oldspace, oldc, E##_t*);				\
+      }									\
+  }
+
+#define ORDERED_TABLE_RESIZE(T, E, type)				\
+  void resize_##T(T##_t *T, arity_t newl)				\
+  {									\
+    arity_t oldc = T->cap, oldl = T->len;				\
+    T->len = newl;							\
+    arity_t newc = pad_alist_size(oldl,newl,oldc,MinCs[type]);		\
+    if (newc != oldc)							\
+      {									\
+	T->cap  = newc;							\
+	T->data = resize_vec(T->data, oldc, newc, E##_t*);		\
+	if (resize_ords( oldl, newl, &T->ocap, MinCs[type], &T->ords, &T->osize )) \
+	  rehash_##T( T->data, newl, newc, T->ords );			\
+      }									\
+  }
+
+#define TABLE_PUT(T, E, K, key, hashfn, cmpfn)		\
+  bool T##_put(T##_t *T, K key, E##_t **buf)		\
+  {							\
+    hash_t  h  = hashfn(key);				\
+    bool    o  = false;					\
+    arity_t m  = T->cap-1;				\
+    arity_t i  = h & m;					\
+    E##_t **E##s = T->data;				\
+    E##_t  *E    = NULL;				\
+							\
     while ((E=E##s[i]) != NULL)				\
       {							\
 	if (E->hash == h && cmpfn(E->key, key) == 0)	\
@@ -113,12 +210,74 @@
     return o;						\
   }
 
+#define ORDERED_TABLE_PUT(T, E, K, key, hashfn, cmpfn)		\
+  bool T##_put(T##_t *T, K key, E##_t **buf)			\
+  {								\
+    hash_t  h  = hashfn(key);					\
+    bool    o  = false;						\
+    arity_t m  = T->cap-1;					\
+    arity_t i  = h & m;						\
+    E##_t **E##s = T->data;					\
+    E##_t  *E    = NULL;					\
+    int32_t ord  = -1;						\
+								\
+    if (T->osize == 1)						\
+      {								\
+	int8_t *ords = T->ords.o8;				\
+	while ((ord=ords[i]) != -1)				\
+	  {							\
+	    E = E##s[ord];					\
+	    if (E->hash == h && cmpfn(E->key, key)==0)		\
+	      break;						\
+	    i = (i+1) & m;					\
+	  }							\
+	if (ord==-1)						\
+	  ords[i] = ord = T->len;				\
+      }								\
+    else if (T->osize == 2)					\
+      {								\
+	int16_t *ords = T->ords.o16;				\
+	while ((ord=ords[i]) != -1)				\
+	  {							\
+	    E = E##s[ord];					\
+	    if (E->hash == h && cmpfn(E->key, key)==0)		\
+	      break;						\
+	    i = (i+1) & m;					\
+	  }							\
+	if (ord==-1)						\
+	  ords[i] = ord = T->len;				\
+      }								\
+    else							\
+      {								\
+	int32_t *ords = T->ords.o32;				\
+	while ((ord=ords[i]) != -1)				\
+	  {							\
+	    E = E##s[ord];					\
+	    if (E->hash == h && cmpfn(E->key, key)==0)		\
+		break;						\
+	    i = (i+1) & m;					\
+	  }							\
+	if (ord==-1)						\
+	  ords[i] = ord = T->len;				\
+      }								\
+    if (ord==T->len)						\
+      {								\
+	o = true;						\
+	E##s[i] = E = new_##E();				\
+	E->hash = h;						\
+	resize_##T(T, T->len+1);				\
+      }								\
+    if (buf)							\
+      *buf = E;							\
+    return o;							\
+  }
+
 #define TABLE_GET(T, E, K, key, hashfn, cmpfn)		\
   bool T##_get(T##_t *T, K key, E##_t **buf)		\
   {							\
     hash_t  h  = hashfn(key);				\
     arity_t m  = T->cap-1;				\
-    arity_t i  = h & m;				\
+    arity_t i  = h & m;					\
     E##_t **E##s = T->data;				\
     E##_t  *E    = NULL;				\
 							\
@@ -135,6 +294,9 @@
   }
 
 // forward declarations
-size_t pad_table_size(size_t oldl, size_t newl, size_t oldc, size_t minc);
+void    init_ords(arity_t ocap, ords_t *ords, arity_t *osize);
+void    free_ords(ords_t ords, arity_t ocap, arity_t osize);
+bool    resize_ords(arity_t oldl, arity_t newl, arity_t *oldc, arity_t minc, ords_t *ords, arity_t *osize);
+arity_t pad_table_size(arity_t oldl, arity_t newl, arity_t oldc, arity_t minc);
 
 #endif
--- src/table.c
+++ src/table.c
@@ -1,11 +1,62 @@
 #include "table.h"
+#include "memory.h"
 
 // general table utilities
 static const double table_resize_pressure = 0.75;
 
-size_t pad_table_size(size_t oldl, size_t newl, size_t oldc, size_t minc)
+void init_ords(arity_t cap, ords_t *ords, arity_t *osize)
 {
-  size_t newc = oldc;
+  if (cap < INT8_MAX)
+    {
+      *(int8_t**)ords = alloc_vec( cap, int8_t );
+      *osize          = 1;
+    }
+
+  else if (cap < INT16_MAX)
+    {
+      *(int16_t**)ords = alloc_vec( cap, int16_t );
+      *osize           = 2;
+    }
+
+  else
+    {
+      *(int32_t**)ords = alloc_vec( cap, int32_t );
+      *osize           = 4;
+    }
+
+  // initialize to -1 of correct width
+  memset(*(byte_t**)ords, 255, cap * *osize );
+}
+
+void free_ords(ords_t ords, arity_t ocap, arity_t osize)
+{
+  if (osize == 1)
+    dealloc_vec( ords.o8, ocap, int8_t );
+
+  else if (osize == 2)
+    dealloc_vec( ords.o16, ocap, int16_t );
+
+  else
+    dealloc_vec( ords.o32, ocap, int32_t );
+}
+
+bool resize_ords(arity_t oldl, arity_t newl, arity_t *oldc, arity_t minc, ords_t *ords, arity_t *osize)
+{
+  arity_t newc = pad_table_size(oldl, newl, *oldc, minc);
+
+  if (newc != *oldc)
+    {
+      free_ords(*ords, *oldc, *osize);
+      init_ords(newc, ords, osize);
+      return true;
+    }
+
+  return false;
+}
+
+arity_t pad_table_size(arity_t oldl, arity_t newl, arity_t oldc, arity_t minc)
+{
+  arity_t newc = oldc;
 
   if (newl > oldl)						
     while (newl > newc*table_resize_pressure)				       
@@ -16,5 +67,4 @@ size_t pad_table_size(size_t oldl, size_t newl, size_t oldc, size_t minc)
   if (newc < minc)
     newc = minc;
   return newc;
-
 }
