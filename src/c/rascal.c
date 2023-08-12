9ca495cd845617d33482a6e2d6c351eacecbc66c
William Jukes <william.r.jukes@gmail.com>
2023-08-12 15:40:03 -0400

    -m

--- src/c/rascal.c
+++ src/c/rascal.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include <string.h>
 #include <stdint.h>
 #include <setjmp.h>
 #include <stdbool.h>
@@ -28,25 +30,46 @@ typedef struct {
   size_t cnt, cap;
 } text_buffer_t;
 
-// internal types -------------------------------------------------------------
+// internal enum types --------------------------------------------------------
 typedef enum type_t {
   NOTYPE,
-  NUMBER
+  NUMBER,
+  UNIT
 } type_t;
 
-typedef struct {
-  jmp_buf jmpbuf;
-} vm_t;
+typedef enum token_t {
+  READY,
+  EXPRESSION,
+  END_OF_STREAM
+} token_t;
 
 // value types ----------------------------------------------------------------
 typedef uintptr_t value_t;
 typedef double number_t;
+
+// internal structure types ---------------------------------------------------
+typedef struct {
+  // reader -------------------------------------------------------------------
+  struct {
+    value_t expression;
+    size_t buffer_size;
+    char* buffer;
+  } reader;
+
+  // error --------------------------------------------------------------------
+  struct {
+    jmp_buf jmpbuf;
+  } error;
+} vm_t;
+
 
 // tags/masks +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #define QNAN 0x7ff8000000000000UL
 #define SIGN 0x8000000000000000UL
 #define TAGM 0xffff000000000000UL
 #define VALM 0x0000ffffffffffffUL
+
+#define NUL  0x7ffc000000000000UL
 
 // size limits ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #define MAXPOW2 0x8000000000000000UL
@@ -55,14 +78,20 @@ typedef double number_t;
 // forward declarations +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // runtime implementations ----------------------------------------------------
 void* allocate(size_t n, bool fromHeap);
+void* allocate_array(size_t n, size_t o, bool fromHeap);
 void* reallocate(void* ptr, size_t oldN, size_t newN, bool fromHeap);
+void* reallocate_array(void* ptr, size_t oldN, size_t newN, size_t o, bool fromHeap);
 void* deallocate(void* ptr, size_t n, bool fromHeap);
+void* deallocate_arraY(void* ptr, size_t n, size_t o, bool fromHeap);
 
 // type implementations -------------------------------------------------------
 // number type ----------------------------------------------------------------
 number_t to_number(const char* fname, value_t value);
 value_t mk_number(number_t number);
 bool is_number(value_t x);
+
+// unit type ------------------------------------------------------------------
+bool is_unit(value_t x);
 
 // internal functions ---------------------------------------------------------
 type_t rl_type_of(value_t v);
@@ -82,6 +111,8 @@ void rl_repl(void);
   } while (false)
 
 #define newline() printf("\n")
+
+#define savepoint() setjmp(Vm.error.jmpbuf)
 
 static inline number_t as_number(value_t val) {
   return ((ieee754_64_t)val).real;
@@ -127,32 +158,71 @@ uint64_t ceilpow2(uint64_t i) {
   void free_##type(ctype* array);                                      \
   void reset_##type(ctype* array);                                     \
   size_t resize_##type(ctype* array, size_t n);                        \
-  size_t type_##push(ctype* array, eltype element);                    \
-  size_t type_##write(ctype* array, size_t n, eltype* elements);       \
-  eltype type_##pop(ctype* array)
+  size_t type##_push(ctype* array, eltype element);                    \
+  size_t type##_write(ctype* array, size_t n, eltype* elements);       \
+  eltype type##_pop(ctype* array)
 
-#define ARRAY_IMPL(type, ctype, eltype, encoded)                       \
-  void init_##type(ctype* array) {                                     \
-    array->cnt  = 0;                                                   \
-    array->cap  = 0;                                                   \
-    array->data = NULL;                                                \
-  }                                                                    \
-                                                                       \
-  void free_##type(ctype* array) {                                     \
-    array->;                                                           \
-  }                                                                    \
-                                                                       \
-  void reset_##type(ctype* array);                                     \
-  size_t resize_##type(ctype* array, size_t n);                        \
-  size_t type_##push(ctype* array, eltype element);                    \
-  size_t type_##write(ctype* array, size_t n, eltype* elements);       \
-  eltype type_##pop(ctype* array)
-
+#define ARRAY_IMPL(type, ctype, eltype, encoded)                        \
+  void init_##type(ctype* array) {                                      \
+    array->cnt  = 0;                                                    \
+    array->cap  = 0;                                                    \
+    array->data = NULL;                                                 \
+  }                                                                     \
+  void free_##type(ctype* array) {                                      \
+    deallocate(array->data, 0, false);                                  \
+  }                                                                     \
+  void reset_##type(ctype* array) {                                     \
+    free_##type(array);                                                 \
+    init_##type(array);                                                 \
+  }                                                                     \
+  size_t resize_##type(ctype* array, size_t n) {                        \
+    rl_require(n <= MAXSIZE, "<runtime @ resize_"#type">", "overflow"); \
+    if (n == 0)                                                         \
+      reset_##type(array);                                              \
+    else {                                                              \
+      size_t padded = n + encoded;                                      \
+      size_t newc   = ceilpow2(padded);                                 \
+      if (newc != array->cap) {                                         \
+        size_t oldc  = array->cap;                                      \
+        size_t els   = sizeof(eltype);                                  \
+        eltype* data = array->data;                                     \
+        array->data  = reallocate_array(data, oldc, newc, els, false);  \
+        array->cap   = newc;                                            \
+      }                                                                 \
+      array->cnt = n;                                                   \
+    }                                                                   \
+    return array->cnt;                                                  \
+  }                                                                     \
+  size_t type##_push(ctype* array, eltype element) {                    \
+    resize_##type(array, array->cnt+1);                                 \
+    array->data[array->cnt-1] = element;                                \
+    return array->cnt;                                                  \
+  }                                                                     \
+  size_t type##_write(ctype* array, size_t n, eltype* elements) {       \
+    assert(elements != NULL);                                           \
+    size_t oldN = array->cnt;                                           \
+    resize_##type(array, array->cnt+n);                                 \
+    eltype* space = array->data + oldN;                                 \
+    memcpy(space, elements, n * sizeof(eltype));                        \
+    return array->cnt;                                                  \
+  }                                                                     \
+  eltype type##_pop(ctype* array) {                                     \
+    rl_require(array->cnt > 0, "<runtime @ "#type"_pop>", "underflow"); \
+    eltype out = array->data[array->cnt-1];                             \
+    resize_##type(array, array->cnt-1);                                 \
+    return out;                                                         \
+  }
 
 // global variables +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 vm_t Vm;
 
 // implementations ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// array implementations ------------------------------------------------------
+ARRAY_API(values, values_t, value_t);
+ARRAY_IMPL(values, values_t, value_t, false);
+ARRAY_API(text_buffer, text_buffer_t, char);
+ARRAY_IMPL(text_buffer, text_buffer_t, char, true);
+
 // type implementations -------------------------------------------------------
 // number type ----------------------------------------------------------------
 SAFECAST(number_t, number, as_number);
@@ -180,10 +250,38 @@ void rl_error(const char* fname, const char* fmt, ...) {
   vfprintf(stderr, fmt, va);
   fprintf(stderr, ".\n");
   va_end(va);
-  longjmp(Vm.jmpbuf, 1);
+  longjmp(Vm.error.jmpbuf, 1);
 }
 
-value_t rl_read(void);
+#define READER_BUFFER_SIZE 512
+
+static void init_reader(void) {
+  Vm.reader.expression = NUL;
+  Vm.reader.buffer_size = READER_BUFFER_SIZE;
+  Vm.reader.buffer = allocate_array(READER_BUFFER_SIZE, , false);
+}
+
+static void free_reader(void) {
+  
+}
+
+static void reset_reader(FILE* src) {
+  
+}
+
+static char* rl_getline(FILE* src) {
+  
+}
+
+value_t rl_read(void) {
+  reset_reader(stdin);
+  char* token = rl_getline(Vm.source);
+
+  if (strcmp(token, "nul") == 0) {
+    Vm.expression = NUL;
+    Vm.token = EXPRESSION;
+  }
+}
 
 value_t rl_eval(value_t v) {
   return v;
@@ -193,6 +291,7 @@ void rl_print(value_t v) {
   type_t type = rl_type_of(v);
   switch (type) {
     case NUMBER: printf("%.2g", as_number(v)); break;
+    case UNIT:   printf("nul"); break;
     default:     printf("#<%s @ %lx>", type_name(type), v); break;
   }
 }
@@ -201,8 +300,8 @@ void rl_print(value_t v) {
 
 void rl_repl(void) {
   while (true) {
-    if (setjmp(Vm.jmpbuf)) {
-      printf("\n");
+    if (savepoint()) {
+      newline();
       continue;
     }
 
