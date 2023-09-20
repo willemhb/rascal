a2c892f8dabd2fd1d92429c41311d284eb6f897c
William <william.r.jukes@gmail.com>
2023-09-20 13:12:11 -0400

    bin??

---
+++ bin/rascal-mode.el
@@ -0,0 +1,38 @@
+;;; package --- Summary:
+
+;;; keeping this here so it's in the repo
+
+;;; Commentary:
+;;; elisp doc formatting is extraordinarily fucked up
+
+;;; Code:
+(define-generic-mode
+  'rascal-mode
+  '("#")
+  '("fun" "mac" "struct" "record" ;; binding syntax
+    "union"                       ;; binding syntax (continued)
+    "perform" "handle" "resume"   ;; effect syntax
+    "catch" "raise" "yield"       ;; control syntax
+    "if" "elif" "else" "case"     ;; branching syntax
+    "cond" "and" "or" "not"       ;; branching syntax (continued)
+    "do" "end"                    ;; do notation
+    "use" "c-call" "quote"        ;; misc syntax
+   )
+  '(("[^:]:[-a-zA-Z?*+0-9/]+" . font-lock-type-face)
+    ("@[-a-zA-Z?*+0-9/]+" . font-lock-type-face)
+    ("::" . font-lock-comment-face)
+    ;; special constants
+    ("\\_<nul\\_>" . font-lock-constant-face)
+    ("\\_<true\\_>" . font-lock-constant-face)
+    ("\\_<false\\_>" . font-lock-constant-face)
+    ;; syntax
+    ("\\_<otherwise\\_>" . font-lock-function-name-face)
+    ("\\_<&\\_>" . font-lock-function-name-face)
+    ;; misc
+    ("\\_<error\\_>" . font-lock-warning-face))
+  '("\\.rascal$" "\\.rascal.d$" "\\.rascal.o$") ;; source file, serialization format, object file
+  (list 'rainbow-delimiters-mode)
+  "A major mode for rascal2 files.")
+
+(provide 'rascal2-mode)
+;;; rascal2-mode.el ends here
