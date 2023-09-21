a74cca7fdb46c44ed9964ffbe107eea4cbe1d311
William <william.r.jukes@gmail.com>
2023-09-21 17:03:32 -0400

    work on hashing.h/c

--- include/hashing.h
+++ include/hashing.h
@@ -4,5 +4,7 @@
 #include "common.h"
 
 uint64_t hashString(const char* chars);
+uint64_t hashBytes(const uint8_t* bytes, size_t n);
+uint64_t hashWord(uint64_t word);
 
 #endif
--- src/hashing.c
+++ src/hashing.c
@@ -1,1 +1,27 @@
 #include "hashing.h"
+
+
+#define FNV_PRIME_64  0x00000100000001B3UL
+#define FNV_OFFSET_64 0xCBF29CE484222325UL
+
+uint64_t hashString(const char* chars) {
+  uint64_t hash = FNV_OFFSET_64;
+
+  for (char ch = *chars; ch != '\0'; chars++, ch=*chars ) {
+    hash ^= ch;
+    hash *= FNV_PRIME_64;
+  }
+
+  return hash;
+}
+
+uint64_t hashBytes(const uint8_t* bytes, size_t n) {
+  uint64_t hash = FNV_OFFSET_64;
+
+  for (size_t i=0; i<n; i++) {
+    hash ^= bytes[i];
+    hash *= FNV_PRIME_64;
+  }
+
+  return hash;
+}
