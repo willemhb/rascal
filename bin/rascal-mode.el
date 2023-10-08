117511b27965f5784a0bcde09ff0f6d1290dafad
William <william.r.jukes@gmail.com>
2023-10-08 19:15:55 -0400

    Just some more syntax experiments

--- bin/rascal-mode.el
+++ bin/rascal-mode.el
@@ -9,17 +9,18 @@
 (define-generic-mode
   'rascal-mode
   '("#")
-  '("module" "fun" "mac" "type" ;; binding syntax
-    "struct" "record" "union"   ;; binding syntax (continued)
-    "begin" "handle"            ;; effect syntax
-    "perform" "resume"          ;; effect syntax (continued)
-    "try" "catch" "finally"     ;; control syntax
-    "raise" "yield"             ;; control syntax (continued)
-    "async" "sync" "await"      ;; control syntax (continued)
-    "if" "elif" "else" "case"   ;; branching syntax
-    "cond" "and" "or" "not"     ;; branching syntax (continued)
-    "do" "end"                  ;; do notation
-    "use" "c-call" "quote"      ;; miscellaneous syntax
+  '("module" "fun" "mac" "type"   ;; binding syntax
+    "struct" "record" "union"     ;; binding syntax (continued)
+    "begin" "handle"              ;; effect syntax
+    "perform" "resume"            ;; effect syntax (continued)
+    "try" "catch" "finally"       ;; control syntax
+    "raise" "yield"               ;; control syntax (continued)
+    "async" "sync" "await"        ;; control syntax (continued)
+    "if" "elif" "else" "case"     ;; branching syntax
+    "cond" "and" "or" "not"       ;; branching syntax (continued)
+    "stx"                         ;; branching syntax (continued)
+    "do" "end"                    ;; do notation
+    "loop" "use" "c-call" "quote" ;; miscellaneous syntax
    )
   '(("[^:]:[-_a-zA-Z?*+0-9/<>]+" . font-lock-type-face)
     ("@[-a-zA-Z?*+0-9/]+" . font-lock-type-face)
@@ -35,6 +36,13 @@
     ("\\_<Any\\_>" . font-lock-type-face)
     ("\\_<None\\_>" . font-lock-type-face)
     ("\\_<Unit\\_>" . font-lock-type-face)
+    ("\\_<Real\\_>" . font-lock-type-face)
+    ("\\_<Small\\_>" . font-lock-type-face)
+    ("\\_<Big\\_>" . font-lock-type-face)
+    ("\\_<Ratio\\_>" . font-lock-type-face)
+    ("\\_<Complex\\_>" . font-lock-type-face)
+    ("\\_<Integer\\_>" . font-lock-type-face)
+    ("\\_<Rational\\_>" . font-lock-type-face)
     ("\\_<Number\\_>" . font-lock-type-face)
     ("\\_<Boolean\\_>" . font-lock-type-face)
     ("\\_<Symbol\\_>" . font-lock-type-face)
--- src/parser.c
+++ src/parser.c
@@ -1,4 +1,4 @@
-#include <stdarg.h>
+ #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
