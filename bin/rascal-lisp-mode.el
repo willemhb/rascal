68db239851f432c1baeaa36f7921eeb3f7246e0a
William <william.r.jukes@gmail.com>
2025-07-04 19:15:18 -0400

    adding rascal lisp mode

---
+++ bin/rascal-lisp-mode.el
@@ -0,0 +1,59 @@
+;;; package --- Summary:
+
+;;; keeping this here so it's in the repo.
+
+;;; Commentary:
+;;; elisp doc formatting is extraordinarily fucked up.
+
+;;; Code:
+(define-generic-mode
+  'rascal-lisp-mode
+  '(";" ("#|" . "|#"))
+  '( ;; special forms
+    ;; binding forms
+    "def" "put" "fn"
+
+    ;; control forms
+    "if" "quote" "do"
+
+    ;; continuations
+    "shift" "reset" "capture"
+
+    ;; miscellaneous forms
+    "annotate"
+
+    ;; extended syntax
+    ;; binding syntax
+    "var" "val" "fun" "mac" "λ" "type" "struct" "record"
+
+    ;; block/looping syntax
+    "let" "label" "loop"
+    
+    ;; conditional syntax
+    "cond" "case" "and" "or" "when" "unless"
+
+    ;; control syntax
+    "try" "handle" "raise"
+    
+    ;; module macros
+    "require" "provide")
+  '(("\\(?:[^-_a-zA-Z?*+0-9/:]:[-_a-zA-Z?*+0-9/]+\\)\\|\\(?:^:[-_a-zA-Z?*+0-9/]+\\)" . font-lock-type-face)
+    ("\\(?:[^~]@[-_a-zA-Z?*+0-9/]+\\)\\|\\(?:^@[-_a-zA-Z?*+0-9/]+\\)" . font-lock-type-face)
+    ;; special constants
+    ("\\_<none\\_>" . font-lock-constant-face)
+    ("\\_<nul\\_>" . font-lock-constant-face)
+    ("\\_<true\\_>" . font-lock-constant-face)
+    ("\\_<false\\_>" . font-lock-constant-face)
+    ;; types
+    ("\\_<[A-Z][a-zA-Z0-9\\|]*\\_>" . font-lock-type-face)
+    ;; syntax
+    ("\\_<otherwise\\_>" . font-lock-function-name-face)
+    ("\\_<&\\_>" . font-lock-function-name-face))
+  '("\\.rl$")
+  (list 'rainbow-delimiters-mode
+                                        ;  (lambda () (set-input-method 'TeX))
+        )
+  "A major mode for rascal files.")
+
+(provide 'rascal-lisp-mode)
+;;; rascal-mode.el ends here
